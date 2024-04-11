128c8b294237b802a74fa1882f3bee8892f5634e
Bekizhan <bekjanabdulzhanov@gmail.com>
2024-04-11 21:15:58 +0600

    Added views to create/edit blog post inside the site.

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from apps.products.models import Post  # Импорт модели Post
+from apps.products.models import Post  
 
-admin.site.register(Post)  # Регистрация модели в админке
+admin.site.register(Post)  
--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 
-class Products(models.Model):  
-    name = models.CharField(max_length=255)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()  
+    created_date = models.DateTimeField(auto_now_add=True)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
 
     def __str__(self):
-        return self.name
+        return self.title
